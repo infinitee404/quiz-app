6288db1f5de6488dc0264c6993c2821c234f742b
infinitee404 <anudeet.74@gmail.com>
2024-04-30 14:39:51 +0545

    First Commit

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,17 @@
+import './toggle.css'
+
+const Toggle = ({ handleChange, isChecked }) => {
+  return (
+    <div className='toggle-container'>
+        <input 
+            type='checkbox'
+            id='check'
+            className='toggle'
+            // onChange={handleChange}
+            // checked={isChecked}
+        />
+    </div>
+  )
+}
+
+export default Toggle
